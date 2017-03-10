a37bf5154aca2721142994809550043302663211
revanth <rev4nth@gmail.com>
2017-03-10 12:02:44 +0530

    add static to the root

---
+++ static/js/main.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+  $('.dropdown-button').dropdown({
+   inDuration: 300,
+   outDuration: 225,
+   constrainWidth: false,
+   hover: false,
+   gutter: 20,
+   belowOrigin: true,
+   alignment: 'right',
+   stopPropagation: false
+   }
+  );
+  $('.tags').material_chip();
+  $('.tags-placeholder').material_chip({
+    secondaryPlaceholder: 'Add a Tag',
+  });
+  
+  $('.tags').on('chip.add', function(e, chip){
+    $('#id_post_tags').val(function(){
+      return this.value + ', ' + chip.tag;
+    });
+  });
+  $('.chips').on('chip.delete', function(e, chip){
+    $('#id_post_tags').val(function(){
+      return (this.value).replace(chip.tag,'');
+    });
+  });
+
+});
