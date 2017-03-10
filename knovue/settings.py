f7b641529b045a2d4ffc98e3eb95b6ce310c7f5a
revanth <rev4nth@gmail.com>
2017-03-10 13:59:08 +0530

    added postgres

--- knovue/settings.py
+++ knovue/settings.py
@@ -103,8 +103,12 @@ LOGIN_URL = '/login'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'knovue',
+        'USER': 'Revanth',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
